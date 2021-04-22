e02ee24d99786cd5a57a55a79bc9863b43ee7e01
Tejashr <tejasteju11@gmail.com>
2021-04-22 14:45:15 +0530

    api commit

--- src/api.js
+++ src/api.js
@@ -1,33 +1,33 @@
 import axios from "axios";
 
 export function PostStudentData(data){
-    return axios.post(`http://localhost:8000/student`,data)
+    return axios.post(`https://studentmentornode.herokuapp.com/student`,data)
 }
 
 export function PostMentorData(data){
-    return axios.post(`http://localhost:8000/mentor`,data)
+    return axios.post(`https://studentmentornode.herokuapp.com/mentor`,data)
 }
 
 export function GetMentorData(){
-    return axios.get(`http://localhost:8000/mentors`)
+    return axios.get(`https://studentmentornode.herokuapp.com/mentors`)
 }
 
 export function GetStudentData(){
-    return axios.get(`http://localhost:8000/students`)
+    return axios.get(`https://studentmentornode.herokuapp.com/students`)
 }
 
 export function GetStudentById(id){
-    return axios.get(`http://localhost:8000/student/${id}`)
+    return axios.get(`https://studentmentornode.herokuapp.com/student/${id}`)
 }
 
 export function GetMentorById(id){
-    return axios.get(`http://localhost:8000/mentor/${id}`)
+    return axios.get(`https://studentmentornode.herokuapp.com/mentor/${id}`)
 }
 
 export function UpdateMentorById(id,data){
-    return axios.put(`http://localhost:8000/mentor/${id}`,data)
+    return axios.put(`https://studentmentornode.herokuapp.com/mentor/${id}`,data)
 }
 
 export function DeleteStudentById(id){
-    return axios.delete(`http://localhost:8000/student/${id}`)
+    return axios.delete(`https://studentmentornode.herokuapp.com/student/${id}`)
 }
